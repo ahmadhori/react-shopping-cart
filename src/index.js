e354ebef9901b8e9dfed3f1de44f9dd0797ee7a7
Ahmad Houri <Ahmad.hori@gmail.com>
2020-01-01 20:26:36 +0100

    Added simple function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const element = <h1>Hello World</h1>;
+const element = <h1>Hello World again</h1>;
 console.log(element);
 ReactDOM.render(element, document.getElementById("root"));
+sayHello();
+
+function sayHello() {
+  for (let index = 0; index < 5; index++) {
+    console.log(index);
+  }
+}
